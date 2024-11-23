5c81a6f4e2d9b32a27866f4182dd1d7b83007853
CatMamaGB <165972971+CatMamaGB@users.noreply.github.com>
2024-11-22 21:27:13 -0600

    Updated errrors

--- Develop/index.js
+++ Develop/index.js
@@ -1,73 +1,142 @@
-// Include packages needed for this application
+// TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require('./utils/generateMarkdown');
+// import {select, Separator} from '@inquirer/prompts'
 
-// Create an array of questions for user input
+console.log(process.argv[1])
+
+// TODO: Create an array of questions for user input
 const questions = [
+    'What is the title of your application?', 
+    'What is the description of your application?',
+    'How do you install your application?',
+    'How should people use your application?',
+    'What are any licenses used to create your application?',
+    'Who are those that contributed to the development of your application?',
+    'What are the test instructions for your application?',
+    'What is your GitHub Username?',
+    'What is your email?'
+];
+
+inquirer.prompt([
     {
         type: 'input',
+        message: questions[0],
         name: 'title',
-        message: 'What is your project title?',
     },
     {
         type: 'input',
+        message: questions[1],
         name: 'description',
-        message: 'Provide a description of your project:',
     },
     {
         type: 'input',
+        message: questions[2],
         name: 'installation',
-        message: 'What are the installation instructions?',
     },
     {
         type: 'input',
+        message: questions[3],
         name: 'usage',
-        message: 'How do you use this application?',
     },
     {
         type: 'list',
-        name: 'license',
-        message: 'Choose a license for your project:',
-        choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+        message: questions[4],
+        name: 'licenses',
+        choices: ['MIT License', 'Mozilla Public License', 'Microsoft Public LIcense', 'Eclipse Public License', 'None'
+        ],
+        
     },
     {
         type: 'input',
-        name: 'contributing',
-        message: 'How can others contribute to this project?',
+        message: questions[5],
+        name: 'contributors',
     },
     {
         type: 'input',
-        name: 'tests',
-        message: 'What are the test instructions?',
+        message: questions[6],
+        name: 'testing',
     },
     {
         type: 'input',
-        name: 'github',
-        message: 'What is your GitHub username?',
+        message: questions[7],
+        name: 'username',
     },
     {
         type: 'input',
+        message: questions[8],
         name: 'email',
-        message: 'What is your email address?',
     },
-];
+])
+.then((response) => {
+    console.log(response)
+    writeToFile(response)
+   
+})
 
-// Create a function to write README file
-function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) =>
-        err ? console.error(err) : console.log('README.md successfully generated!')
-    );
+
+
+// TODO: Create a function to write README file
+function writeToFile(data) {
+    let license = data.licenses
+    let description
+         if (data.licenses == 'MIT License') {
+            description = 'The MIT License is an older license used to provide copyright protection.';
+        } else if (data.licenses == 'Mozilla Public License') {
+            description = 'The Mozilla Public License is used by Mozilla and Firefox. It provides copywrite protection and can be used in commercial applications.';
+        } else if (data.licenses == 'Microsoft Public LIcense') {
+            description = 'The Microsoft derives code that can be destributed for both commercial and non-commercial use. Any code that is redistributed must included a complete copy of the Microsoft Public License.';
+        } else if (data.licenses == 'Eclipse Public License') {
+            description = 'The Eclipse Public License is a newer version of the Common Public License, which allows it to be more acceptable to business.';
+        } else if (data.licenses == 'None') {
+            license = '';
+            description = 'Please refer to the LICENSE in the repo.';
+        }
+const fileContent = `
+# ${data.title}
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Licenses](#licenses)
+- [Contributors](#contributors)
+- [Testing](#testing)
+- [Contact](#contact-info-for-questions)
+
+## Description
+${data.description}
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Licenses
+#### ${license}
+${description}
+
+## Contributors
+${data.contributors}
+
+## Testing
+${data.testing}
+
+## Contact info for Questions
+#### Github: 
+<a href="https://github.com/${data.username}">${data.username}</a>
+
+#### Email: 
+<a href="mailto: ${data.email}">${data.email}</a>`
+
+
+    fs.writeFile('README.md', fileContent, (err) =>
+    err ? console.error(err) : console.log('Success!') )
 }
 
-// Create a function to initialize app
-function init() {
-    inquirer.prompt(questions)
-        .then((answers) => {
-            const markdownContent = generateMarkdown(answers);
-            writeToFile('README.md', markdownContent);
-        });
-}
+// TODO: Create a function to initialize app
+function init() {}
 
 // Function call to initialize app
 init();
